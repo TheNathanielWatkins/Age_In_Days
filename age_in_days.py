c65f0e8ff01c0283118fee4599c3f0409026c898
Nathaniel Watkins <TheNathanielWatkins@gmail.com>
2016-11-08 15:14:28 -0800

    Add files via upload
    Getting close to a working version with command line input.

--- age_in_days.py
+++ age_in_days.py
@@ -4,7 +4,11 @@
 # Simply put, if you were born 1 Jan 2012 and todays date is
 # 2 Jan 2012 you are 1 day old.
 
+import time
+
 daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+special_message = " "
 
 def isLeapYear(year):
     if year % 4 != 0:
@@ -46,19 +50,6 @@ def calc_whole_years(y1, y2):
 
 def daysBetweenDates(y1, m1, d1, y2, m2, d2):
     days = 0
-    # if (y1.is_integer() is False) or (m1.is_integer() is False) or (d1.is_integer() is False) or (y2.is_integer() is False) or (m2.is_integer() is False) or (d2.is_integer() is False):
-    #     if y1.is_integer() is False:
-    #         return "Hey!  What's the big idea?!  You should know that's not how dates are formatted.  Check the birthYEAR and try again."
-    #     if m1.is_integer() is False:
-    #         return "Hey!  What's the big idea?!  You should know that's not how dates are formatted.  Check the birthMONTH and try again."
-    #     if d1.is_integer() is False:
-    #         return "Hey!  What's the big idea?!  You should know that's not how dates are formatted.  Check the birthDAY and try again."
-    #     if y2.is_integer() is False:
-    #         return "Hey!  What's the big idea?!  You should know that's not how dates are formatted.  Check the current YEAR and try again."
-    #     if m2.is_integer() is False:
-    #         return "Hey!  What's the big idea?!  You should know that's not how dates are formatted.  Check the current MONTH and try again."
-    #     if d2.is_integer() is False:
-    #         return "Hey!  What's the big idea?!  You should know that's not how dates are formatted.  Check the current DAY and try again."
     if (y1 <= 0) or (m1 <= 0) or (d1 <= 0) or (y2 <= 0) or (m2 <= 0) or (d2 <= 0):
         if y1 <= 0:
             return "Hey!  What's the big idea?!  You should know that's not how dates are formatted.  Check the birthYEAR and try again."
@@ -81,9 +72,10 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
             return "Sorry, there seems to be an issue with the day that you entered. Current day could not possibly be {0} since {1}/{2} only has 29 days in it.".format(d2,m2,y2)
         return "Sorry, there seems to be an issue with the day that you entered. Current day could not possibly be {0} since {1}/{2} only has {3} days in it.".format(d2,m2,y2,daysOfMonths[m2-1])
     if (y1 == y2) and (m1 == m2) and (d1 == d2):
-        return "Thank your mother because you were just born today!"
+        special_message = "Thank your mother because you were just born today!"
+        return 0
     if (m1 == m2) and (d1 == d2):
-        print "Happy Birthday!! You're now {0} years old!".format(y2-y1)
+        special_message = "Happy Birthday!! You're now {0} years old!".format(y2-y1)
     if y1 > y2:
         return "Sorry, there seems to be an issue with the years that you entered. Birthyear cannot be after the current year. ...unless you're some kind of Time Lord."
     if y1 == y2:
@@ -108,9 +100,68 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
     else:
         return "Congratulations! You win. You've found some inputs that I didn't plan for (and somehow they didn't cause an error). Let me know how you did it. K THX BYE"
 
-# print "#1 " + str(daysBetweenDates(-2017,-01,-01,-2016,-01,-01))
-# print "#2 " + str(daysBetweenDates(0,0,0,0,0,0))
-# print "#3 " + str(daysBetweenDates(2016,02,29,2016,02,29))
-# print "#4 " + str(daysBetweenDates(2013,01,01,2015,01,01))
-# print "#5 " + str(daysBetweenDates(2010.0,03.0,01,2020,03,01))
-# print "#6 " + str(daysBetweenDates(1988,06,0,2016,11,07))
+print "Ready to find out how many day's you've been alive?  Please input your birthdate then the current date in this format: YYYY,MM,DD"
+
+while True:
+	try:
+		y1 = int(input("Enter Birth Year > "))
+		if y1 > 0:
+		    break
+	except:
+		print("The year needs to be a positive whole number")
+while True:
+	try:
+		m1 = int(input("Enter Birth Month > "))
+		if (m1 >= 1) and (m1 <= 12):
+		    break
+	except:
+		print("The month needs to be a whole number between 1 and 12")
+while True:
+    try:
+        d1 = int(input("Enter Birth Day > "))
+        if (d1 >= 1) and (d1 <= 31):
+            break
+    except:
+		print("The day needs to be a whole number between 1 and 31")
+
+print "Today's date (formatted YYYY,MM,DD) is " + (time.strftime("%Y,%m,%d"))
+toggle = True
+answer = " "
+while toggle:
+    answer = input("Would you like to auto-input today's date? [Y/N] If no, you'll be prompted to enter the current date. > ")
+    # answer = answer.lower()
+    # print answer
+    if (answer.lower() == "y") or (answer.lower() == "yes") or (answer.lower() == "yeah") or (answer.lower() == "sure") or (answer.lower() == "affirmative") or (answer.lower() == "yup") or (answer.lower() == "okay") or (answer.lower() == "ok"):
+        y2 = time.strftime("%Y")
+        m2 = time.strftime("%m")
+        d2 = time.strftime("%d")
+        toggle = False
+    elif (answer.lower() == "n") or (answer.lower() == "no") or (answer.lower() == "nah") or (answer.lower() == "nope") or (answer.lower() == "negative") or (answer.lower() == "nix"):
+        while True:
+            try:
+                y2 = int(input("Enter Current Year > "))
+                if y2 > 0:
+                    break
+            except:
+        		print("The year needs to be a positive whole number")
+        while True:
+        	try:
+        		m2 = int(input("Enter Current Month > "))
+        		if (m2 >= 1) and (m1 <= 12):
+        		    break
+        	except:
+        		print("The month needs to be a whole number between 1 and 12")
+        while True:
+            try:
+                d2 = int(input("Enter Current Day > "))
+                if (d2 >= 1) and (d1 <=31):
+                    break
+            except:
+        		print("The day needs to be a whole number between 1 and 31")
+        toggle = False
+    else:
+        print 'Your response was not recognized as a "Yes" or "no", please try again.'
+
+daysBetweenDates(y1,m1,d1,y2,m2,d2)
+print special_message
+print "Congratulations! You've now been alive for a whoping {0} days!".format(daysBetweenDates(y1,m1,d1,y2,m2,d2))
